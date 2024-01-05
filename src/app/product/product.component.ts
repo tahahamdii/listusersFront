bdbe390d555e8fb993679bee4c46c42e73eab4ca
Med Taha Hamdi <hamdi.taha@esprit.tn>
2024-01-05 14:50:33 +0100

    @Input

--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Product } from '../model/product';
 
 @Component({
   selector: 'app-product',
   templateUrl: './product.component.html',
   styleUrls: ['./product.component.css']
 })
-export class ProductComponent {
+export class ProductComponent implements OnInit{
+  @Input() product: Product;
+  constructor() {}
+
+  ngOnInit(): void {
+    
+  }
 
 }
