0af0b7989b7b6acad31434a8a17bc3e41755a69f
Med Taha Hamdi <hamdi.taha@esprit.tn>
2024-01-08 17:50:27 +0100

    adding works on server

--- src/app/add-product/add-product.component.ts
+++ src/app/add-product/add-product.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { Product } from '../model/product';
 import { Productt } from '../model/product2';
+import { ProductService } from '../product.service';
 
 @Component({
   selector: 'app-add-product',
@@ -10,5 +11,16 @@ import { Productt } from '../model/product2';
 export class AddProductComponent {
 
   product: Productt = new Productt();
+
+  constructor(private productService: ProductService){}
+
+  addProduct(){
+    this.productService.addProduct(this.product).subscribe(
+      response => {
+        this.product = new Productt();
+        alert('product added habibi')
+      }
+    )
+  }
 
 }
